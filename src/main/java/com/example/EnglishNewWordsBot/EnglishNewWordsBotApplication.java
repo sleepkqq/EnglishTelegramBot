c5bada195760bfad7fb41010276ecfd0a73f2e7a
sleepkqq <nikita32ivanov@gmail.com>
2023-04-03 17:44:07 +0300

    Initial commit

---
+++ src/main/java/com/example/EnglishNewWordsBot/EnglishNewWordsBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.EnglishNewWordsBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishNewWordsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishNewWordsBotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/EnglishNewWordsBot/service/TelegramBot.java
@@ -0,0 +1,67 @@
+package com.example.EnglishNewWordsBot.service;
+
+import com.example.EnglishNewWordsBot.config.BotConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    final BotConfig config;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+
+            switch (messageText) {
+                case "/start":
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                default:
+                    sendMessage(chatId, "жопа");
+            }
+        }
+    }
+
+    private void startCommandReceived(long chatId, String name) {
+
+        String answer = "hi" + name;
+
+        sendMessage(chatId, answer);
+    }
+
+    private void sendMessage(long chatId, String textToSend) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText(textToSend);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+
+        }
+    }
+
+    @Autowired
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+}
